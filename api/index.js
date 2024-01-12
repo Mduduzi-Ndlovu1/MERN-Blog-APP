bcdaf41855ad0b0a8380e4faf1e0e21854511856
Mduduzindlovu <Mduduzindlovu61@gmail.com>
2024-01-12 21:57:43 +0200

    user Authentication in progress

--- api/index.js
+++ api/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdasdasdasd'
+
 
 const app = express();
 
@@ -23,13 +29,35 @@ mongoose.connect('mongodb+srv://mduduzindlovu61:C0gH3601PsKRPEcn@cluster0.ur44j2
 app.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body;
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({ username, password:bcrypt.hashSync(password,salt) });
     res.json(userDoc);
   } catch (error) {
     console.error('Error registering user:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.post('/login',async (req,res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({username});
+
+    const passOk = bcrypt.compareSync(password,userDoc.password);
+    res.json(passOk);
+
+    if (passOk) {
+        jwt.sign({
+            username,
+            id:userDoc._id
+        },secret,{},(error,token) => {
+            if (error) {
+                throw error
+            };
+            res.json(token)
+        })
+    } else {
+        res.status(400).json('wrong Credentials') // 1:17:06
+    }
+})
 
 const PORT = process.env.PORT || 4000;
 
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function login(e){
+    fetch('http://localhost:4000/login',{
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' }, // Corrected typo here
+    })
+  }
   return (
     <div>
         
-        <form className='login'>
+        <form className='login'  onSubmit={login}>
         <h1>Log in</h1>
-            <input type="text" placeholder='Username' />
-            <input type="password" placeholder='Password' />
-            <button>Login</button>
+        <input
+          type="text"
+          placeholder='Username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+            <button type='submit'>Login</button>
         </form>
     </div>
   )
 }
 
 export default LoginPage
+
